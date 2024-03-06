748eee1e470d6fcb988159d97174b4554fcaa4aa
ThunderBolt-OS <soham.bhoir@somaiya.edu>
2024-03-06 16:54:13 +0530

    rm peer channels

--- config.py
+++ config.py
@@ -2,12 +2,26 @@
 import os
 from dotenv import load_dotenv
 
+# load the environment variables
 load_dotenv()
 
-# load the environment variables
+# telegram variables
 TELEGRAM_API_ID = os.getenv("TELEGRAM_API_ID")
 TELEGRAM_API_HASH = os.getenv("TELEGRAM_API_HASH")
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_BOT_USENAME = os.getenv("TELEGRAM_BOT_USENAME")
 TELEGRAM_BOT_NAME = os.getenv("TEMEGRAM_BOT_NAME")
 TELEGRAM_PHONE_NUMBER = os.getenv("TELEGRAM_PHONE_NUMBER")
+
+
+# firebase variables
+FIREBASE_apiKey = os.getenv("FIREBASE_apiKey")
+FIREBASE_authDomain = os.getenv("FIREBASE_authDomain")
+FIREBASE_databaseURL = os.getenv("FIREBASE_databaseURL")
+FIREBASE_projectId = os.getenv("FIREBASE_projectId")
+FIREBASE_storageBucket = os.getenv("FIREBASE_storageBucket")
+FIREBASE_messagingSenderId = os.getenv("FIREBASE_messagingSenderId")
+FIREBASE_appId = os.getenv("FIREBASE_appId")
+FIREBASE_measurementId = os.getenv("FIREBASE_measurementId")
+
+# print(TELEGRAM_API_ID, TELEGRAM_API_HASH, TELEGRAM_BOT_TOKEN, TELEGRAM_BOT_USENAME, TELEGRAM_BOT_NAME, TELEGRAM_PHONE_NUMBER, FIREBASE_apiKey, FIREBASE_authDomain, FIREBASE_databaseURL, FIREBASE_projectId, FIREBASE_storageBucket, FIREBASE_messagingSenderId, FIREBASE_appId, FIREBASE_measurementId)
--- main.py
+++ main.py
@@ -1,65 +1,56 @@
 from config import TELEGRAM_API_ID, TELEGRAM_API_HASH, TELEGRAM_PHONE_NUMBER
 from telethon.errors import SessionPasswordNeededError
 from telethon import TelegramClient, events, sync
-from telethon.tl.functions.messages import (GetHistoryRequest)
-from telethon.tl.types import (PeerChannel)
+from telethon.tl.functions.messages import GetHistoryRequest
+from telethon.tl.types import PeerChannel
 
 api_id = TELEGRAM_API_ID
 api_hash = TELEGRAM_API_HASH
-
-user_input_channel = 'https://t.me/+8auXr4xpMnxhMWZl'
-
+user_input_channel = "https://t.me/+8auXr4xpMnxhMWZl"
 global_event = None
 
 
-# listener
 async def main():
-    client = TelegramClient('session_name', api_id, api_hash)
+    client = TelegramClient("session_name", api_id, api_hash)
     await client.start()
     print("Client Created")
     if not await client.is_user_authorized():
         await client.send_code_request(TELEGRAM_PHONE_NUMBER)
         try:
-            await client.sign_in(TELEGRAM_PHONE_NUMBER, input('Enter the code: '))
+            await client.sign_in(TELEGRAM_PHONE_NUMBER, input("Enter the code: "))
         except SessionPasswordNeededError:
-            await client.sign_in(password=input('Password: '))
+            await client.sign_in(password=input("Password: "))
 
     me = await client.get_me()
-    print(me.stringify())
+    print(f"Welcome: {me.first_name}, your ID is: {me.id}, and your username is: @{me.username}")
     channel = user_input_channel
-    # Convert it to a PeerChannel
-    entity = await client.get_entity(channel)
-    # How many messages to retrieve
-    limit = 1
-    # The offset ID for which we retrieve the messages
-    offset_id = 0
-    # The maximum ID for which we retrieve the messages
-    max_id = 0
-    messages = await client(GetHistoryRequest(
-        peer=entity,
-        limit=limit,
-        offset_date=None,
-        offset_id=offset_id,
-        max_id=max_id,
-        min_id=0,
-        add_offset=0,
-        hash=0
-    ))
+    # entity = await client.get_entity(channel)   # Convert it to a PeerChannel
+    # limit = 1                                   # How many messages to retrieve
+    # offset_id = 0                               # The offset ID for which we retrieve the messages
+    # max_id = 0                                  # The maximum ID for which we retrieve the messages
+    # messages = await client(
+    #     GetHistoryRequest(
+    #         peer=entity,
+    #         limit=limit,
+    #         offset_date=None,
+    #         offset_id=offset_id,
+    #         max_id=max_id,
+    #         min_id=0,
+    #         add_offset=0,
+    #         hash=0,
+    #     )
+    # )
 
-
-    # Add an event handler for new messages
-    @client.on(events.NewMessage(chats=channel))
+    @client.on(
+        events.NewMessage(chats=channel)
+    )  # Add an event handler for new messages
     async def handler(event):
         global_event = event
         print(global_event.raw_text)
 
-    print('Listening')
+    print("Listening")
     await client.run_until_disconnected()
 
-with TelegramClient('anon', api_id, api_hash) as client:
+
+with TelegramClient("anon", api_id, api_hash) as client:
     client.loop.run_until_complete(main())
-    
-
-
-
-
